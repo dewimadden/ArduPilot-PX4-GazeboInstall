0043173671d0e2cfdaf1278751aa3b81d8315185
djmads08 <47112428+djmads08@users.noreply.github.com>
2021-06-29 16:03:25 +0100

    Add files via upload

---
+++ InstallRowdenTestBed.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+mkdir Test_Development
+cd Test_Development
+echo "STEP 1/10 Installing Git Gui" 
+echo "===================="
+sudo apt-get update
+sudo apt-get install
+sudo apt-get install gitk git-gui
+echo "Git GUI Successfully Installed"
+
+echo "STEP 2/10 Now installing Ardupilot"
+echo "===================="
+git clone https://github.com/ArduPilot/ardupilot.git
+echo "navigating to ardupilot directory"
+cd ardupilot
+git submodule update --init --recursive
+
+echo "STEP 3/10 Fixing pre-requisites"
+echo "===================="
+cd Tools/environment_install/
+install-prereqs-ubuntu.sh -y
+
+echo "STEP 4/10 resetting directory and user profile"
+echo "===================="
+. ~/.profile
+
+echo "STEP 5/10 Performing clean build"
+echo "===================="
+cd ~
+cd ardupilot
+./waf clean
+
+echo "STEP 6/10 Let's go back to the Test Development Directory"
+echo "===================="
+cd ~
+cd Test_Development
+
+echo "STEP 7/10 Now let's install Gazebo"
+echo "===================="
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt update
+
+sudo apt install gazebo9 libgazebo9-dev
+
+echo "STEP 8/10 Time to install your plugins for ArduPilot"
+echo "===================="
+git clone https://github.com/khancyr/ardupilot_gazebo
+cd ardupilot_gazebo
+mkdir build
+cd build
+cmake ..
+make -j4
+sudo make install
+
+echo "STEP 9/10 Resetting directory and user profile"
+echo "===================="
+. ~/.profile
+cd ~
+
+echo "STEP 10/10 I will now install PixHawk 4"
+echo "===================="
+cd Test_Development
+git clone https://github.com/PX4/PX4-Autopilot.git --recursive
+bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
+
+echo "I now need to restart your computer, please restart your machine"
+echo "===================="
+exec bash
+
+
